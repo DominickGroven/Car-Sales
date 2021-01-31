927b1dd42c216705628abfb253f725ec3000085c
Dominick Groven <36062458+DominickGroven@users.noreply.github.com>
2021-01-31 08:37:04 -0800

    MVP

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-const Header = props => {
+import { connect } from "react-redux";
+const Header = (props) => {
   return (
     <>
       <figure className="image is-128x128">
@@ -12,4 +12,10 @@ const Header = props => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
